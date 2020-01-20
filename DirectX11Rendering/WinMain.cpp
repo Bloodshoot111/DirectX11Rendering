49bb2db23470bbdbe57186b0ba16078d85bb7890
Niklas <niklas28mayer@yahoo.de>
2020-01-20 16:49:04 +0100

    Added some test Callbacks

--- DirectX11Rendering/WinMain.cpp
+++ DirectX11Rendering/WinMain.cpp
@@ -1,6 +1,30 @@
 #include <Windows.h>
-#include <string>
+#include <sstream>
 
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	
+	switch(msg)
+	{
+	case WM_CLOSE:
+		PostQuitMessage(0);
+		break;
+	case WM_KEYDOWN:
+		if(wParam == 'F')
+		{
+			SetWindowText(hWnd,L"Test");
+		}
+		break;
+	case WM_CHAR:
+	{
+		static std::wstring title;
+		title.push_back(char(wParam));
+		SetWindowText(hWnd, title.c_str());
+	}
+		break;
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
 
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
@@ -17,7 +41,7 @@ int CALLBACK WinMain(
 	WNDCLASSEX wc = { 0 };
 	wc.cbSize = sizeof(wc);
 	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = DefWindowProc;
+	wc.lpfnWndProc = WndProc;
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = hInstance;
@@ -35,9 +59,19 @@ int CALLBACK WinMain(
 		nullptr, nullptr, hInstance, nullptr);
 	//show the window
 	ShowWindow(hWnd, SW_SHOW);
-	while(true)
+	//message pump
+	MSG msg;
+	BOOL gResult;
+	while((gResult = GetMessage(&msg,nullptr, 0,0)) > 0)
 	{
-		
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
 	}
-	return 0;
+	if(gResult == -1)
+	{
+		return -1;
+	} else
+	{
+		return msg.wParam;
+	}
 }
