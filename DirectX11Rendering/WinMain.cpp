f01d1feaa6ef8460d74dfcfd12ac1dba19bb369a
Niklas <niklas28mayer@yahoo.de>
2020-01-21 15:01:14 +0100

    Encapsulated Window Creation

--- DirectX11Rendering/WinMain.cpp
+++ DirectX11Rendering/WinMain.cpp
@@ -1,77 +1,27 @@
-#include <Windows.h>
-#include <sstream>
+#include "CustomWin.h"
+#include "Window.h"
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-	
-	switch(msg)
-	{
-	case WM_CLOSE:
-		PostQuitMessage(0);
-		break;
-	case WM_KEYDOWN:
-		if(wParam == 'F')
-		{
-			SetWindowText(hWnd,L"Test");
-		}
-		break;
-	case WM_CHAR:
-	{
-		static std::wstring title;
-		title.push_back(char(wParam));
-		SetWindowText(hWnd, title.c_str());
-	}
-		break;
-	}
-	return DefWindowProc(hWnd, msg, wParam, lParam);
-}
 
-int CALLBACK WinMain(
+int WINAPI WinMain(
 	HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
 	LPSTR lpCmdLine,
 	int nCmdShow)
 {
-	const std::wstring className = L"DX11";
-	const std::wstring windowName = L"DX11Window";
-	const LPCWSTR pClassName = className.c_str();
-	const LPCWSTR lpWindowName = windowName.c_str();
-	
-	//register window class
-	WNDCLASSEX wc = { 0 };
-	wc.cbSize = sizeof(wc);
-	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = WndProc;
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
-	wc.hInstance = hInstance;
-	wc.hIcon = nullptr;
-	wc.hCursor = nullptr;
-	wc.hbrBackground = nullptr;
-	wc.lpszMenuName = nullptr;
-	wc.lpszClassName = pClassName;
-	wc.hIconSm = nullptr;
-	RegisterClassEx(&wc);
-	//Create Window Instance
-	HWND hWnd = CreateWindowEx(
-		0, pClassName, lpWindowName,
-		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,  200, 200, 640, 480,
-		nullptr, nullptr, hInstance, nullptr);
-	//show the window
-	ShowWindow(hWnd, SW_SHOW);
-	//message pump
+	Window wnd(1024, 680, L"DirectX 11 Engine");
+
 	MSG msg;
 	BOOL gResult;
-	while((gResult = GetMessage(&msg,nullptr, 0,0)) > 0)
+	while((gResult = GetMessage(&msg,nullptr,0,0)) > 0)
 	{
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
+
 	if(gResult == -1)
 	{
 		return -1;
-	} else
-	{
-		return msg.wParam;
 	}
+
+	return msg.wParam;
 }
---
+++ DirectX11Rendering/Window.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "CustomWin.h"
+
+class Window
+{
+private:
+	//singleton manages registration/cleanup of window class
+	class WindowClass
+	{
+	public:
+		static const wchar_t* GetName() noexcept;
+		static HINSTANCE GetInstance() noexcept;
+
+	private:
+		WindowClass() noexcept;
+		~WindowClass();
+		WindowClass(const WindowClass&) = delete;
+		WindowClass& operator=(const Window&) = delete;
+		static constexpr const wchar_t* wndCLassName = L"DirectX11 Engine Window";
+		static WindowClass wndClass;
+		HINSTANCE hInst;
+	};
+public:
+	Window(int width, int height, const wchar_t* name) noexcept;
+	~Window();
+	Window(const Window&) = delete;
+	Window& operator=(const Window&) = delete;
+private:
+	static LRESULT WINAPI HandleMsgSetup(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept;
+	static LRESULT WINAPI HandleMsgThunk(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lparam) noexcept;
+	LRESULT HandleMsg(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept;
+private:
+	int width;
+	int height;
+	HWND hWnd;
+};
